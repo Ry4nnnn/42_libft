1e6c2538f95b6d1b690b4c7b4c094af361fdd9ee
Wei jun Lim <welim@u91z04s02.42kl.edu.my>
2022-04-16 15:01:29 +0800

    part 1

--- ft_split.c
+++ ft_split.c
@@ -1,16 +1,15 @@
 #include "libft.h"
 
-
-static size_t	word(char const *s, char c)
+static size_t	count(char const *s, char c)
 {
-	int i;
-	int count;
+	size_t i;
+	size_t count;
 
 	i = 0;
 	count = 0;
 	while (s[i] != '\0')
 	{
-		while (s == c)
+		while (s[i] == c)
 			i++;
 		if (s[i] != c && s[i])
 			count += 1;
@@ -20,22 +19,31 @@ static size_t	word(char const *s, char c)
 	return (count);
 }
 
-
-char **ft_split(char const *s, char c)
+char	**ft_split(char const	*s, char	c)
 {
-	char **res;
+	char	**res;
 	size_t	i;
 	size_t	j;
+	size_t	k;
 
-	i = 0;
-	j = 0;
 	if (!s)
 		return (0);
-	res = (char **)malloc(sizeof(s) *word(s, c) + 1);
+	res = (char **)malloc(sizeof(s) * count(s, c) + 1);
 	if (!res)
 		return (0);
-	while (s[i] != c)
+	i = 0;
+	k = 0;
+	while (s[i] != '\0' && k < count(s, c))
 	{
-		
+		j = 0;
+		while (s[i] == c)
+			i++;
+		while (s[i + j] != c && s[i + j] != '\0')
+			j++;
+		res[k] = (char *)malloc(sizeof(*s) * (j + 1));
+		ft_strlcpy(res[k++], s + i, j + 1);
+		i += j;
 	}
+	res[k] = 0;
+	return (res);
 }
