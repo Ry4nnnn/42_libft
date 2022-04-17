21eb8e605080204c3d034e814c9ef970e1848d74
Wei jun Lim <welim@u80z03s08.42kl.edu.my>
2022-04-17 23:15:27 +0800

    will do header soon

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -5,8 +5,8 @@ t_list *ft_lstnew(void *content)
 	t_list	*res;
 
 	res = (t_list *)malloc(sizeof(t_list));
-	// if (!res)
-	// 	return (0);
+	if (!res)
+		return (0);
 	res->content = content;
 	res->next = 0;
 	return (res);
